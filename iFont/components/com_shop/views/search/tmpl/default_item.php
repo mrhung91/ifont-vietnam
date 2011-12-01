81487ac34ddb1b553838037a9fb3e3f6df6bd744
duongthienduc <duongthienduc@a20a5037-b0da-d8ea-0815-7e5c09b06159>
2011-12-01 07:21:11 +0000

    Implement search function

---
+++ iFont/components/com_shop/views/search/tmpl/default_item.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version		$Id: default_articles.php 21700 2011-06-28 04:32:41Z dextercowley $
+ * @package		Joomla.Site
+ * @subpackage	com_content
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+$doc = JFactory::getDocument();
+
+$item_id = $this->item->id;
+?>
+<div class="ofh mg_bot30px">
+	<div class="headindex">
+		<ul class="left">
+			<li class="fix_txt"><?php echo $this->item->name; ?></li>
+		</ul>
+		<?php if (!$this->item->isFontAdded) : ?>
+		<ul class="right">
+			<li class="fix_txt">
+				<input name="" value="" type="checkbox"><span><?php echo number_format($this->item->price, 0, '', "."); ?></span>
+			</li>
+			<li class="btn_buy">
+				<a id="lnkBuyFont<?php echo $item_id; ?>" href="javascript:;"
+					onclick="buyFont(this, <?php echo $item_id; ?>)">MUA</a>
+			</li>
+		</ul>
+		<?php endif; ?>
+	</div>
+	<div class="show_img_font" id="font-sample-<?php echo $item_id; ?>">
+		<img />
+	</div>
+</div>
---
+++ iFont/components/com_shop/views/search/view.html.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * version $Id: view.html.php 21484 2011-06-08 00:57:51Z dextercowley $
+ * @package		Joomla.Site
+ * @subpackage	com_content
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the Content component
+ *
+ * @package		Joomla.Site
+ * @subpackage	com_content
+ * @since 1.5
+ */
+class ShopViewSearch extends JView
+{
+	protected $state;
+	protected $items;
+	protected $pagination;
+
+	function display($tpl = null)
+	{
+		$app	= JFactory::getApplication();
+		$user	= JFactory::getUser();
+
+		// Get some data from the models
+		$state			= $this->get('State');
+		$params			= $state->params;
+		$items			= $this->get('Items');
+		$filterSearch	= $this->get('FilterSearch');
+		$pagination = $this->get('Pagination');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+
+		// Compute the article slugs and prepare introtext (runs content plugins).
+		foreach ($items as $item) {
+			$item->slug = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
+		}
+
+		//Escape strings for HTML output
+		$this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
+
+		$this->assignRef('state', $state);
+		$this->assignRef('items', $items);
+		$this->assignRef('params', $params);
+		$this->assignRef('pagination', $pagination);
+		$this->assignRef('user', $user);
+		$this->assignRef('Itemid',	JRequest::getInt('Itemid', 0));
+		$this->assignRef('filterSearch',	$filterSearch);
+
+		$this->_prepareDocument();
+
+		parent::display($tpl);
+	}
+
+	/**
+	 * Prepares the document
+	 */
+	protected function _prepareDocument()
+	{
+		$app		= JFactory::getApplication();
+		$menus		= $app->getMenu();
+		$pathway	= $app->getPathway();
+		$title		= null;
+
+		// Because the application sets a default page title,
+		// we need to get it from the menu item itself
+		$menu = $menus->getActive();
+
+		if ($menu) {
+			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+		} else {
+			$this->params->def('page_heading', JText::_('COM_SHOP_SEARCH_FONTS'));
+		}
+
+		$id = (int) @$menu->query['id'];
+
+		$title = $this->params->get('page_title', '');
+
+		if (empty($title)) {
+			$title = $app->getCfg('sitename');
+		}
+		elseif ($app->getCfg('sitename_pagetitles', 0) == 1) {
+			$title = JText::sprintf('JPAGETITLE', $app->getCfg('sitename'), $title);
+		}
+		elseif ($app->getCfg('sitename_pagetitles', 0) == 2) {
+			$title = JText::sprintf('JPAGETITLE', $title, $app->getCfg('sitename'));
+		}
+
+		$this->document->setTitle($title);
+	}
+}
