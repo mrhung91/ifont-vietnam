19b8d35625c9b3fb21136b3a110362c28167fbca
duongthienduc <duongthienduc@a20a5037-b0da-d8ea-0815-7e5c09b06159>
2011-12-02 16:34:44 +0000

    Revised login box's css

--- trunk/iFont/modules/mod_login/tmpl/default.php
+++ trunk/iFont/modules/mod_login/tmpl/default.php
@@ -18,29 +18,30 @@ $cartUrl = JRoute::_("index.php?option=com_shop&view=cart&Itemid=185");
 ?>
 <?php if ($type == 'logout') : ?>
 <div class="welcome">
-<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form">
-<?php if ($params->get('greeting')) : ?>
-	<div class="login-greeting">
-	<?php if($params->get('name') == 0) : {
-		echo JText::sprintf('MOD_LOGIN_HINAME', $user->get('name'));
-	} else : {
-		echo JText::sprintf('MOD_LOGIN_HINAME', $user->get('username'));
-	} endif; ?>
+	<div class="user-info clearfix">
+		<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form">
+		<?php if ($params->get('greeting')) : ?>
+			<div class="login-greeting">
+			<?php if($params->get('name') == 0) : {
+				echo JText::sprintf('MOD_LOGIN_HINAME', $user->get('name'));
+			} else : {
+				echo JText::sprintf('MOD_LOGIN_HINAME', $user->get('username'));
+			} endif; ?>
+			</div>
+		<?php endif; ?>
+			<div class="logout-button">
+				<input type="submit" name="Submit" class="button" value="<?php echo JText::_('JLOGOUT'); ?>" />
+				<input type="hidden" name="option" value="com_users" />
+				<input type="hidden" name="task" value="user.logout" />
+				<input type="hidden" name="return" value="<?php echo $return; ?>" />
+				<?php echo JHtml::_('form.token'); ?>
+			</div>
+		</form>
 	</div>
-<?php endif; ?>
-	<div class="logout-button">
-		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('JLOGOUT'); ?>" />
-		<input type="hidden" name="option" value="com_users" />
-		<input type="hidden" name="task" value="user.logout" />
-		<input type="hidden" name="return" value="<?php echo $return; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
-</form>
-<br />
-<div class="cart-info">
-	<a href="<?php echo $cartUrl; ?>">Giỏ hàng</a>:&nbsp;
-	<span id="lblNumCartFonts"><?php echo $num_fonts; ?></span> phông,
-	<span id="lblNumCartPackages"><?php echo $num_packages; ?></span> gói phông
+	<div class="cart-info">
+		<a href="<?php echo $cartUrl; ?>">Giỏ hàng</a>:&nbsp;
+		<span id="lblNumCartFonts"><?php echo $num_fonts; ?></span> phông,
+		<span id="lblNumCartPackages"><?php echo $num_packages; ?></span> gói phông
 	</div>
 </div>
 <?php else : ?>
