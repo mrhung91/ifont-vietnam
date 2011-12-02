3da89584350cb9ac805b8cebe28723a707e9dcec
duongthienduc <duongthienduc@a20a5037-b0da-d8ea-0815-7e5c09b06159>
2011-12-02 16:42:07 +0000

    Revised checkout page's layout

--- iFont/components/com_shop/views/cart/tmpl/default.php
+++ iFont/components/com_shop/views/cart/tmpl/default.php
@@ -16,43 +16,54 @@ $packages = $this->items["packages"];
 $total = 0;
 ?>
 <div class="cart-info">
-<?php if ($this->params->get('show_page_heading', 1)) : ?>
+	<?php if ($this->params->get('show_page_heading', 1)) : ?>
 	<div class="heading_bderbot">
 		<h3 class="textright">
 			<a href="javascript:;" onclick="history.go(-1);" class="shop-back"></a>
 			<?php echo $this->escape($this->params->get('page_heading')); ?></h3>
 	</div>
-<?php endif; ?>
+	<?php endif; ?>
 
-<?php if (count($fonts) > 0 || count($packages) > 0) : ?>
-<div class="table font-list">
-	<?php foreach ($fonts as $index => $item): ?>
-	<?php
-		$this->item = $item;
-		echo $this->loadTemplate('item');
-		$total += $item->price;
-	?>
-	<?php endforeach; ?>
-	<?php foreach ($packages as $index => $item): ?>
-	<?php
-		$this->item = $item;
-		echo $this->loadTemplate('package');
-		$total += $item->price;
-	?>
-	<?php endforeach; ?>
-</div>
-<div class="ofh fix02">
-	<div class="link_green fl">
-		<a href="<?php echo JURI::base(); ?>">Chọn thêm phông</a> &gt;&gt;
+	<?php if (count($fonts) > 0 || count($packages) > 0) : ?>
+	<div class="table font-list">
+		<?php foreach ($fonts as $index => $item): ?>
+		<?php
+			$this->item = $item;
+			echo $this->loadTemplate('item');
+			$total += $item->price;
+		?>
+		<?php endforeach; ?>
+		<?php foreach ($packages as $index => $item): ?>
+		<?php
+			$this->item = $item;
+			echo $this->loadTemplate('package');
+			$total += $item->price;
+		?>
+		<?php endforeach; ?>
 	</div>
-	<div class="fr">
-		Tổng<span class="price"><?php echo number_format($total, 0, '', "."); ?> VNĐ</span>
+	<div class="ofh fix02">
+		<div class="link_green fl">
+			<a href="<?php echo JURI::base(); ?>">Chọn thêm phông</a> &gt;&gt;
+		</div>
+		<div class="fr">
+			Tổng<span class="price"><?php echo number_format($total, 0, '', "."); ?> VNĐ</span>
+		</div>
 	</div>
-</div>
+	<div class="fix03">
+		<a class="btn_buy_big" href="javascript:;" onclick="checkOutCart()">MUA</a>
+	</div>
 
-<?php else: ?>
-<div class="no-result">
-Không có phông nào trong giỏ hàng.
-</div>
-<?php endif; ?>
+	<form id="cartForm" name="cartForm" method="post"
+			action="<?php echo JRoute::_("index.php?option=com_shop&task=cart.checkout"); ?>">
+		<?php echo JHtml::_('form.token'); ?>
+	</form>
+
+	<script type="text/javascript">
+	function checkOutCart() {
+		$("#cartForm").submit();
+	}
+	</script>
+	<?php else: ?>
+	<div class="no-result">Không có phông nào trong giỏ hàng.</div>
+	<?php endif; ?>
 </div>
