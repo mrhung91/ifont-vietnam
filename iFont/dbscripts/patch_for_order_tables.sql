4960b9ea46de8ba3b77b36e8d7d2c25e530c2593
duongthienduc <duongthienduc@a20a5037-b0da-d8ea-0815-7e5c09b06159>
2011-12-07 12:42:13 +0000

    Added script to create order tables

---
+++ iFont/dbscripts/patch_for_order_tables.sql
@@ -0,0 +1,67 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.02 
+MySQL - 5.1.41 : Database - ifont
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `if_shop_order` */
+
+DROP TABLE IF EXISTS `if_shop_order`;
+
+CREATE TABLE `if_shop_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(6) NOT NULL,
+  `state` tinyint(2) NOT NULL,
+  `total` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(11) NOT NULL,
+  `modified` datetime DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ifso_user_created_by` (`created_by`),
+  KEY `FK_ifso_user_modified_by` (`modified_by`),
+  CONSTRAINT `FK_ifso_user_created_by` FOREIGN KEY (`created_by`) REFERENCES `if_users` (`id`),
+  CONSTRAINT `FK_ifso_user_modified_by` FOREIGN KEY (`modified_by`) REFERENCES `if_users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+/*Table structure for table `if_shop_order_history` */
+
+DROP TABLE IF EXISTS `if_shop_order_history`;
+
+CREATE TABLE `if_shop_order_history` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) NOT NULL,
+  `status` int(2) NOT NULL DEFAULT '0',
+  `comment` varchar(1024) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `if_shop_order_item` */
+
+DROP TABLE IF EXISTS `if_shop_order_item`;
+
+CREATE TABLE `if_shop_order_item` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) NOT NULL,
+  `font_id` int(10) DEFAULT NULL,
+  `package_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_sod_so_order_id` (`order_id`),
+  KEY `FK_soi_sp_package_id` (`package_id`),
+  KEY `FK_if_sod_sf_font_id` (`font_id`),
+  CONSTRAINT `FK_sod_so_order_id` FOREIGN KEY (`order_id`) REFERENCES `if_shop_order` (`id`),
+  CONSTRAINT `FK_if_sod_sf_font_id` FOREIGN KEY (`font_id`) REFERENCES `if_shop_font` (`font_id`),
+  CONSTRAINT `FK_soi_sp_package_id` FOREIGN KEY (`package_id`) REFERENCES `if_shop_package` (`package_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=208 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
